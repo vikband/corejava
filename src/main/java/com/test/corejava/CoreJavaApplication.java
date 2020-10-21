2448ac33456ac9824ebb3535cf9bc7e9c134cfdc
Your Name <vikrambandarupalli04@gmail.com>
2020-10-20 22:33:34 -0700

    adding project structure

---
+++ src/main/java/com/test/corejava/CoreJavaApplication.java
@@ -0,0 +1,13 @@
+package com.test.corejava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreJavaApplication.class, args);
+	}
+
+}
